96a85faf2ce29344df0511981a4363467418de44
yangshaobo96 <416329952@qq.com>
2017-04-20 17:32:17 +0800

    FINISH

---
+++ code/main.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+class Solution {
+public:
+    int poorPigs(int buckets, int minutesToDie, int minutesToTest) {
+        int time=minutesToTest/minutesToDie+1;
+        double res=log(buckets)/log(time);
+        if(res>(int)res) return (int)(res+1);
+        return (int)res;
+    }
+};
+int main(int argc, char *argv[])
+{
+    cout << "Hello World!" << endl;
+    return 0;
+}
