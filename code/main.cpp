8efcf5f32dc163b6d7d7c5003a52876de959bee2
yangshaobo96 <416329952@qq.com>
2017-04-20 18:00:33 +0800

    FINISH

--- code/main.cpp
+++ code/main.cpp
@@ -1,18 +1,29 @@
 ﻿#include <iostream>
 #include <cmath>
+#include <string>
+#include <vector>
+#include <cstring>
 
 using namespace std;
 class Solution {
 public:
-    int poorPigs(int buckets, int minutesToDie, int minutesToTest) {
-        int time=minutesToTest/minutesToDie+1;
-        double res=log(buckets)/log(time);
-        if(res>(int)res) return (int)(res+1);
-        return (int)res;
+    string longestCommonPrefix(vector<string>& strs) {
+        if(strs.size()==0) return "";
+        string s=strs.back();
+        strs.pop_back();
+        int i=0;
+        for(;i<s.length();++i){
+            for(string j:strs){
+                if(i>=j.length()||j[i]!=s[i]) return s.substr(0,i);
+            }
+        }
+        if(i==s.length()) return s;
+        return "";
     }
 };
-int main(int argc, char *argv[])
-{
-    cout << "Hello World!" << endl;
+int main(int argc, char *argv[]){
+    vector<string> s{"c","c"};
+    Solution a;
+    string res=a.longestCommonPrefix(s);
     return 0;
 }
